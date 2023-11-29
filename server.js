af1cb2815eb484be854ee65856d2a4ae236867e7
ManikantaSirangu <87496228+ManikantaSirangu@users.noreply.github.com>
2023-11-29 14:34:01 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get("/",function(req,res){
+    res.render("index");
+});
+
+
+app.get('/output', (req, res) => {
+  res.sendFile(__dirname + '/output');
+});
+
+// Handle form submission
+app.post('/output', (req, res) => {
+  const { noun, verb, adjective, adverb, place } = req.body;
+
+  // Your Mad Lib template
+  const madLib = `Once upon a time, in a ${adjective} land, there were ${noun}s who loved to ${verb} ${adverb} around the ${place}.`;
+
+  // Send back the filled Mad Lib
+  res.send(madLib);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
